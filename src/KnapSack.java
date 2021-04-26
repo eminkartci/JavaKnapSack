b8381c97a44aada374f76a23ce661a88552da030
eminkartci <emin.kartci@ozu.edu.tr>
2021-04-26 12:10:22 +0300

    KnapSack is implemented

--- src/KnapSack.java
+++ src/KnapSack.java
@@ -1,6 +1,27 @@
 
+// KnapSack
+	// Reading & Generating the items
+
 public class KnapSack {
 	
 	
+	// Program starts here
+	public static void main(String[] args) {
+		
+		Data problemData = ReadData();
 
+		
+	}
+
+	// Read data and create item array and return them
+	private static Data ReadData() {
+		
+		// initialize
+		Data problemData = new Data();
+		
+
+		return problemData;
+		
+	}
+	
 }
