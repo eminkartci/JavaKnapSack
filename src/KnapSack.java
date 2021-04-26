1aed5dbe5927a2413bd512b72c4937d2f92561c4
eminkartci <emin.kartci@ozu.edu.tr>
2021-04-26 12:19:34 +0300

    Read item data from csv

--- src/KnapSack.java
+++ src/KnapSack.java
@@ -1,4 +1,11 @@
 
+// IMPORTS
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 // KnapSack
 	// Reading & Generating the items
 
@@ -19,6 +26,34 @@ public class KnapSack {
 		// initialize
 		Data problemData = new Data();
 		
+		// Read data from csv file 
+		try {
+			BufferedReader bufferedReader=new BufferedReader(new FileReader(new File("/Users/eminkartci/eclipse-workspace/JavaKnapSack/src/data.csv")));
+			
+			// Read first line
+			String line = bufferedReader.readLine();
+			
+			// split the line wrt comma
+			String[] item_id_array = line.split(",");
+			
+			line = bufferedReader.readLine();
+			
+			String[] item_value_array = line.split(",");
+			
+			line = bufferedReader.readLine();
+			
+			String[] item_value_weight = line.split(",");
+			
+			line = bufferedReader.readLine();
+			
+			String[] item_value_time = line.split(",");
+			
+			System.out.println(line);
+		
+		// Inform the user
+		} catch (Exception e ) {
+			e.printStackTrace();
+		}
 
 		return problemData;
 		
